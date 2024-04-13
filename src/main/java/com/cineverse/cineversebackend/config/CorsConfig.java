b8a2c0ad67102f6fabb23f0b3687b27eb05e379d
조수빈 <angelajsb@naver.com>
2024-04-13 22:05:26 +0900

    pipeline final test

--- src/main/java/com/cineverse/cineversebackend/config/CorsConfig.java
+++ src/main/java/com/cineverse/cineversebackend/config/CorsConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//push test
+// pipeline final test
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
