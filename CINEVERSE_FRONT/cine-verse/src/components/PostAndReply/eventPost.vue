75d2703718a0ae8cdc0cf22edbf99dfcec2002c8
Jihye1101 <yang2jihye@gmail.com>
2024-04-16 12:05:36 +0900

    이벤트게시판 페이지 라우터 푸시 추가함
    이벤트 게시글 클릭하면 해당 이벤트 게시글 페이지로 넘어갈 수 있게 코드 추가함

--- CINEVERSE_FRONT/cine-verse/src/components/PostAndReply/eventPost.vue
+++ CINEVERSE_FRONT/cine-verse/src/components/PostAndReply/eventPost.vue
@@ -1,91 +1,92 @@
 <template>
-    <div class="wrapper">
-        <section>
-            <div class="clickdiv" @click="mainBoard()">
-                <h1 class="boardname">자유 게시판</h1> 
-            </div>
-            <hr class="boardtitleLine">
-        </section>
-        <div class="allboard" >
-            <div class="boardtitlediv">
-                <h2 class="boardtitle"> {{ event.eventTitle }}</h2>
-            </div>
-            <div class="datediv">
-                <h3 class="date">{{ event.eventDate }}</h3>
-            </div>
-            <div class="like">
-           <button type="button" @click="editPost">
-                수정
-            </button>
-        </div>
-            <div class="writerdiv">
-                <h3 class="writer">{{ event.member.nickname }}</h3>
-            </div>
-            <div class="viewandlike">
-                <h3 class="viewandlike">{{ event.eventViewCount }}</h3>
-            </div>
-        </div>
-        <hr class="titleLine">
-        <div class="maincontent">
-            <p v-html="event.eventContent"></p>
-            <div v-if="event.images && event.images.length > 0" class="image-container">
-                <img v-for="(image, index) in event.images" :key="index" :src="image.accessUrl" :alt="'Image ' + (index + 1)" class="event-image">
-            </div>
-        </div>
-        <Like/>
-        <div class="like">
-           <button type="button" @click="deletePost">
-                삭제
-            </button>
-        </div>
-        <Reply/>
+  <div class="wrapper">
+    <section>
+      <div class="clickdiv" @click="mainBoard()">
+        <h1 class="boardname">이벤트 게시판</h1>
+      </div>
+      <hr class="boardtitleLine">
+    </section>
+    <div class="allboard">
+      <div class="boardtitlediv">
+        <h2 class="boardtitle"> {{ event.eventTitle }}</h2>
+      </div>
+      <div class="datediv">
+        <h3 class="date">{{ event.eventDate }}</h3>
+      </div>
+      <div class="like">
+        <button type="button" @click="editPost">
+          수정
+        </button>
+      </div>
+      <div class="writerdiv">
+        <h3 class="writer">{{ event.member.nickname }}</h3>
+      </div>
+      <div class="viewandlike">
+        <h3 class="viewandlike">{{ event.eventViewCount }}</h3>
+      </div>
     </div>
+    <hr class="titleLine">
+    <div class="maincontent">
+      <p v-html="event.eventContent"></p>
+      <div v-if="event.images && event.images.length > 0" class="image-container">
+        <img v-for="(image, index) in event.images" :key="index" :src="image.accessUrl" :alt="'Image ' + (index + 1)"
+          class="event-image">
+      </div>
+    </div>
+    <Like />
+    <div class="like">
+      <button type="button" @click="deletePost">
+        삭제
+      </button>
+    </div>
+    <Reply />
+  </div>
 </template>
 
 <script setup>
-    import router from '@/router/mainRouter';
-    import Reply from './Reply.vue';
-    import Like from './Like.vue';
-    import axios from "axios";
-    import { ref, onBeforeMount } from "vue";
-    import { useRoute } from "vue-router";
+import router from '@/router/mainRouter';
+import Reply from './Reply.vue';
+import Like from './Like.vue';
+import axios from "axios";
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
 
-    const event = ref({});
-    const eventId = useRoute();
+const event = ref({});
+const eventId = useRoute();
 
-    const fetchevent = () => {
-        axios.get(`http://localhost:8081/event_board/${eventId.params.eventId}`)
-        .then(response => {
-            event.value = response.data;
-        })
-        .catch(error => {
-            console.error("Error fetching event:", error);
-        });
-    }
-
-    onBeforeMount(() => {
-        fetchevent();
+const fetchevent = () => {
+  axios.get(`http://localhost:8081/event_board/${eventId.params.eventId}`)
+    .then(response => {
+      event.value = response.data;
+    })
+    .catch(error => {
+      console.error("Error fetching event:", error);
     });
+}
 
-    function mainBoard() {
-        router.push('/event_board/list');
-    }
+onBeforeMount(() => {
+  fetchevent();
+});
 
-    function deletePost() {
-        axios.patch(`http://localhost:8081/event_board/delete/${eventId.params.eventId}`)
-        .then(response => {
-            console.log('게시물이 성공적으로 삭제되었습니다.');
-            router.push('/event_board/list');
-        })
-        .catch(error => {
-            console.error("Error deleting post:", error);
-        });
-    }
- 
+function mainBoard() {
+  router.push('/event_board/list');
+}
+
+function deletePost() {
+  axios.patch(`http://localhost:8081/event_board/delete/${eventId.params.eventId}`)
+    .then(response => {
+      console.log('게시물이 성공적으로 삭제되었습니다.');
+      router.push('/event_board/list');
+    })
+    .catch(error => {
+      console.error("Error deleting post:", error);
+    });
+}
+
 </script>
 
 <style scoped>
-@import url('@/assets/css/postAndReply/eventPost.css');   
+@import url('@/assets/css/postAndReply/eventPost.css');
 </style>
 
 <!-- <template>
--- CINEVERSE_FRONT/cine-verse/src/components/PostMain/eventPostMain.vue
+++ CINEVERSE_FRONT/cine-verse/src/components/PostMain/eventPostMain.vue
@@ -9,19 +9,19 @@
                     <button class="votebtn">퀴즈</button>
                 </div>
                 <div class="voteboxesdiv">
-                    <div class="voteboxdiv1" v-if="events.length > 0">
+                    <div class="voteboxdiv1" v-if="events.length > 0" @click="gotoEvent(events[0].eventBoardId)">
                         <div class="votebox1">{{ events[0].title }}</div>
                         <div class="votebox2">{{ events[0].content }}
                             <button class="like1" @click="toggleLike(1)" :class="{ liked: isLiked[0] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="voteboxdiv2" v-if="events.length > 1">
+                    <div class="voteboxdiv2" v-if="events.length > 1" @click="gotoEvent(events[1].eventBoardId)">
                         <div class="votebox3">{{ events[1].title }}</div>
                         <div class="votebox4">{{ events[1].content }}
                             <button class="like2" @click="toggleLike(2)" :class="{ liked: isLiked[1] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="voteboxdiv3" v-if="events.length > 2">
+                    <div class="voteboxdiv3" v-if="events.length > 2" @click="gotoEvent(events[2].eventBoardId)">
                         <div class="votebox5">{{ events[2].title }}</div>
                         <div class="votebox6">{{ events[2].content }}
                             <button class="like3" @click="toggleLike(3)" :class="{ liked: isLiked[2] }">♥ 좋아요</button>
@@ -34,19 +34,19 @@
                     <!-- <button class="quizbtn">투표</button> -->
                 </div>
                 <div class="quizboxesdiv">
-                    <div class="quizboxdiv1" v-if="events.length > 3">
+                    <div class="quizboxdiv1" v-if="events.length > 3" @click="gotoEvent(events[3].eventBoardId)">
                         <div class="quizbox1">{{ events[3].title }}</div>
                         <div class="quizbox2">{{ events[3].content }}
                             <button class="like4" @click="toggleLike(4)" :class="{ liked: isLiked[3] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="quizboxdiv2" v-if="events.length > 4">
+                    <div class="quizboxdiv2" v-if="events.length > 4" @click="gotoEvent(events[4].eventBoardId)">
                         <div class="quizbox3">{{ events[4].title }}</div>
                         <div class="quizbox4">{{ events[4].content }}
                             <button class="like5" @click="toggleLike(5)" :class="{ liked: isLiked[4] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="quizboxdiv3" v-if="events.length > 5">
+                    <div class="quizboxdiv3" v-if="events.length > 5" @click="gotoEvent(events[5].eventBoardId)">
                         <div class="quizbox5">{{ events[5].title }}</div>
                         <div class="quizbox6">{{ events[5].content }}
                             <button class="like6" @click="toggleLike(6)" :class="{ liked: isLiked[5] }">♥ 좋아요</button>
@@ -72,25 +72,35 @@ function toggleLike(buttonNumber) {
     isLiked.value[buttonNumber - 1] = !isLiked.value[buttonNumber - 1];
 }
 
-const defaultEvent = { title: '이벤트 제목 없음', content: '이벤트 내용 없음' };
-
-const events = computed(() => {
-    return info.value.map(event => ({
-        title: event.eventTitle || '이벤트 제목 없음',
-        content: event.eventContent || '이벤트 내용 없음',
-    }));
-});
-
-
 onMounted(async () => {
     try {
         const response = await axios.get("http://localhost:8081/event_board/list");
-        // 날짜 기준 내림차순: 최신 이벤트가 배열 처음에
+        // 날짜 기준 내림차순
         info.value = response.data.sort((a, b) => new Date(b.eventDate) - new Date(a.eventDate));
     } catch (error) {
         console.error('이벤트 정보 전송 시에 에러 발생하였습니다.', error);
     }
 });
+
+const events = computed(() => {
+    return info.value.map(event => ({
+        eventBoardId: event.eventBoardId, 
+        title: event.eventTitle || '이벤트 제목 없음',
+        content: event.eventContent || '이벤트 내용 없음',
+    }));
+});
+
+function gotoEvent(eventBoardId) {
+    if (eventBoardId) {
+        router.push(`/event_board/${eventBoardId}`).catch(err => {
+            if (err.name !== 'NavigationDuplicated') {
+                console.error(err);
+            }
+        });
+    } else {
+        console.error('eventBoardId가 정의되지 않았습니다.');
+    }
+}
 
 
 </script>
