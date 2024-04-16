f6291de47773496aa07b5e73dc878dd76e95ab29
조수빈 <angelajsb@naver.com>
2024-04-16 19:34:03 +0900

    젠킨스 테스트 7 - 최종 테스트2

--- src/main/java/com/cineverse/cineversebackend/review/board/controller/ReviewBoardController.java
+++ src/main/java/com/cineverse/cineversebackend/review/board/controller/ReviewBoardController.java
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-/* 젠킨스 주석 */
 @RestController
 @RequestMapping("/review_board")
 public class ReviewBoardController {
