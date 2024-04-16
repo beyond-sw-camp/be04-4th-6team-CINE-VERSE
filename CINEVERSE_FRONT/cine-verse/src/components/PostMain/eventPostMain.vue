16667482f6e3029bbb8d82b7fc4b049ebbe7d2c0
Jihye1101 <yang2jihye@gmail.com>
2024-04-16 12:09:19 +0900

    이벤트 게시판 페이지 수정함
    이벤트 제목을 클릭할 때 해당 이벤트 게시글로 라우터 푸시되고, 이벤트 내용이 있는 칸의 좋아요를 누르면 좋아요 버튼이 눌리고 다시 한 번 누르면 해제됨

--- CINEVERSE_FRONT/cine-verse/src/components/PostMain/eventPostMain.vue
+++ CINEVERSE_FRONT/cine-verse/src/components/PostMain/eventPostMain.vue
@@ -9,20 +9,20 @@
                     <button class="votebtn">퀴즈</button>
                 </div>
                 <div class="voteboxesdiv">
-                    <div class="voteboxdiv1" v-if="events.length > 0" @click="gotoEvent(events[0].eventBoardId)">
-                        <div class="votebox1">{{ events[0].title }}</div>
+                    <div class="voteboxdiv1" v-if="events.length > 0">
+                        <div class="votebox1" @click="gotoEvent(events[0].eventBoardId)">{{ events[0].title }}</div>
                         <div class="votebox2">{{ events[0].content }}
                             <button class="like1" @click="toggleLike(1)" :class="{ liked: isLiked[0] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="voteboxdiv2" v-if="events.length > 1" @click="gotoEvent(events[1].eventBoardId)">
-                        <div class="votebox3">{{ events[1].title }}</div>
+                    <div class="voteboxdiv2" v-if="events.length > 1">
+                        <div class="votebox3" @click="gotoEvent(events[1].eventBoardId)">{{ events[1].title }}</div>
                         <div class="votebox4">{{ events[1].content }}
                             <button class="like2" @click="toggleLike(2)" :class="{ liked: isLiked[1] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="voteboxdiv3" v-if="events.length > 2" @click="gotoEvent(events[2].eventBoardId)">
-                        <div class="votebox5">{{ events[2].title }}</div>
+                    <div class="voteboxdiv3" v-if="events.length > 2">
+                        <div class="votebox5" @click="gotoEvent(events[2].eventBoardId)">{{ events[2].title }}</div>
                         <div class="votebox6">{{ events[2].content }}
                             <button class="like3" @click="toggleLike(3)" :class="{ liked: isLiked[2] }">♥ 좋아요</button>
                         </div>
@@ -34,20 +34,20 @@
                     <!-- <button class="quizbtn">투표</button> -->
                 </div>
                 <div class="quizboxesdiv">
-                    <div class="quizboxdiv1" v-if="events.length > 3" @click="gotoEvent(events[3].eventBoardId)">
-                        <div class="quizbox1">{{ events[3].title }}</div>
+                    <div class="quizboxdiv1" v-if="events.length > 3">
+                        <div class="quizbox1" @click="gotoEvent(events[3].eventBoardId)">{{ events[3].title }}</div>
                         <div class="quizbox2">{{ events[3].content }}
                             <button class="like4" @click="toggleLike(4)" :class="{ liked: isLiked[3] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="quizboxdiv2" v-if="events.length > 4" @click="gotoEvent(events[4].eventBoardId)">
-                        <div class="quizbox3">{{ events[4].title }}</div>
+                    <div class="quizboxdiv2" v-if="events.length > 4">
+                        <div class="quizbox3" @click="gotoEvent(events[4].eventBoardId)">{{ events[4].title }}</div>
                         <div class="quizbox4">{{ events[4].content }}
                             <button class="like5" @click="toggleLike(5)" :class="{ liked: isLiked[4] }">♥ 좋아요</button>
                         </div>
                     </div>
-                    <div class="quizboxdiv3" v-if="events.length > 5" @click="gotoEvent(events[5].eventBoardId)">
-                        <div class="quizbox5">{{ events[5].title }}</div>
+                    <div class="quizboxdiv3" v-if="events.length > 5">
+                        <div class="quizbox5" @click="gotoEvent(events[5].eventBoardId)">{{ events[5].title }}</div>
                         <div class="quizbox6">{{ events[5].content }}
                             <button class="like6" @click="toggleLike(6)" :class="{ liked: isLiked[5] }">♥ 좋아요</button>
                         </div>
